32d480ac07510f2e59de731ae6e03ae47be6b540
daves2ng <daves2ng@gmail.com>
2023-03-13 23:31:12 +0100

    modified the create-artifacts.sh to create the organizations as well as the channels at the same time

--- execution-scripts-files/create-artifacts.sh
+++ execution-scripts-files/create-artifacts.sh
@@ -1,35 +1,35 @@
 
 chmod -R 0755 ./crypto-config
 # Delete existing artifacts
-rm -rf ./crypto-config
-rm genesis.block mychannel.tx
-rm -rf ../../channel-artifacts/*
+# rm -rf ./crypto-config
+# rm genesis.block mychannel.tx
+# rm -rf ../../channel-artifacts/*
 
-#Generate Crypto artifactes for organizations
+# #Generate Crypto artifactes for organizations
 /home/daves2ng/fabric-samples/bin/cryptogen generate --config=./crypto-config.yaml --output=./crypto-config/
 
 
 
 # System channel
-#SYS_CHANNEL="sys-channel"
+SYS_CHANNEL="sys-channel"
 
 # channel name defaults to "mychannel"
-#CHANNEL_NAME="zonechannel"
+CHANNEL_NAME="zonechannel"
 
-# echo $CHANNEL_NAME
+ echo $CHANNEL_NAME
 
 # # Generate System Genesis block
-# /home/daves2ng/fabric-samples/bin/configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
+/home/daves2ng/fabric-samples/bin/configtxgen -profile OrdererGenesis -configPath . -channelID $SYS_CHANNEL  -outputBlock ./genesis.block
 
 
 # # Generate channel configuration block
-# /home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
+/home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputCreateChannelTx ./mychannel.tx -channelID $CHANNEL_NAME
 
 # echo "#######    Generating anchor peer update for GTBMSP  ##########"
-# /home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./GTBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg GTBMSP
+/home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./GTBMSPanchors.tx -channelID $CHANNEL_NAME -asOrg GTBMSP
 
 # echo "#######    Generating anchor peer update for WemaMSP  ##########"
-# /home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./WemaMSPanchors.tx -channelID $CHANNEL_NAME -asOrg WemaMSP
+/home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./WemaMSPanchors.tx -channelID $CHANNEL_NAME -asOrg WemaMSP
 
 # echo "#######    Generating anchor peer update for ZenithMSP  ##########"
-# /home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ZenithMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ZenithMSP
+/home/daves2ng/fabric-samples/bin/configtxgen -profile BasicChannel -configPath . -outputAnchorPeersUpdate ./ZenithMSPanchors.tx -channelID $CHANNEL_NAME -asOrg ZenithMSP
